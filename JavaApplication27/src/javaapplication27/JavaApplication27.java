fb5a48193759a37f5a00ac8066d1616543b3b3fc
Jakub Silhan <jakub.silhan@tul.cz>
2022-03-15 13:59:13 +0100

    Test successful

--- JavaApplication27/src/javaapplication27/JavaApplication27.java
+++ JavaApplication27/src/javaapplication27/JavaApplication27.java
@@ -5,6 +5,7 @@ public class JavaApplication27 {
 
     public static void main(String[] args) {
         System.out.println("Jen neco testuju");
+        System.out.println("Tak to asi funguje");
     }
     
 }
